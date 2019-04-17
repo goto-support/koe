6bdba4a7cc0684e2f21466566e2554445fc199e5
gotouTatsumi <gotogotoborn@gmail.com>
2019-04-17 11:26:20 +0900

    no message

--- run.js
+++ run.js
@@ -96,7 +96,6 @@ $(function () {
       var href = $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top;
-      console.log(position);
       $('body,html').animate({ scrollTop: position }, speed, 'swing');
       return false;
     });
